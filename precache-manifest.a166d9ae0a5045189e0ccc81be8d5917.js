0ef047cacde685ee6c9e1611d7991a3af8f3149c
Komisarov-D <62189887+komisarov-d@users.noreply.github.com>
2020-07-20 21:20:34 +0300

    Updates

---
+++ precache-manifest.a166d9ae0a5045189e0ccc81be8d5917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efa7e4d4bfd9084d1fcb09ce46e0bd7",
+    "url": "/portfolio_social-network/index.html"
+  },
+  {
+    "revision": "6bc83f95d1613f69fd0f",
+    "url": "/portfolio_social-network/static/css/main.71eddfae.chunk.css"
+  },
+  {
+    "revision": "2c430965c82ef20fdd91",
+    "url": "/portfolio_social-network/static/js/2.a31eced7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/portfolio_social-network/static/js/2.a31eced7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc83f95d1613f69fd0f",
+    "url": "/portfolio_social-network/static/js/main.16830fc5.chunk.js"
+  },
+  {
+    "revision": "b35971d5cf51b81bc629",
+    "url": "/portfolio_social-network/static/js/runtime-main.712578e1.js"
+  }
+]);
